9c1eff4b762c5526a5bf9e960aa8459edf4cb325
miao0007 <miao188@hotmail.com>
2020-10-09 22:21:27 +1030

    HeaderSaved Component

---
+++ client/src/components/HeaderSaved/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+class HeaderSaved extends React.Component {
+    render() {
+        return (
+            <div className="saved-header">
+            <h1>Saved Books</h1>
+            <h2>View your Saved Books Below</h2>
+        </div>
+        )
+    }
+}

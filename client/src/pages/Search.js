2fd63fc9edaa3730538d476d592a3743bd8b402c
miao0007 <miao188@hotmail.com>
2020-10-10 14:20:51 +1030

    build Search page

---
+++ client/src/pages/Search.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Header from "../components/Header";
+import Results from "../components/Result";
+import API from "../utils/API";
+import ReactDOM from "react-dom";
+
+class Search extends React.Component {
+    state = {
+        books: [],
+        search: "",
+        status: "Search Book!"
+    }
+    handleSearch = (search) => {
+        API.searchBooks(search).then(res => {
+            this.setState({books: res.data, status: "Search Results: "});
+            console.log(this.state);
+            const results = ReactDOM.findDOMNode(this.refs.test);
+            results.scrollIntoView({behavior: 'smooth'});
+        })
+    }
+    updateInput = (event) => {
+        this.setState({
+            search: event.target.value
+        })
+    }
+    componentWillUnmount(){
+        API.deleteUnsaved()
+    }
+
+    render() {
+        return (
+            <div>
+                <Header handleSearch = {this.handleSearch} search = {this.state.search} updateInput = {this.updateInput} />
+                <div ref = "test">
+                    <Results books = {this.state.books} status = {this.state.status} buttonText = "Save" />
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Search

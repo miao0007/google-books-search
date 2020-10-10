06fe07bd36f15ae1d4a1c157e13f477194687c65
miao0007 <miao188@hotmail.com>
2020-10-10 15:05:10 +1030

    create saved page

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,34 @@
+import React from "react";
+import HeaderSaved from "../components/HeaderSaved";
+import Results from "../components/Result";
+import API from "../utils/API";
+
+class Saved extends React.Component {
+    state = {
+        saved: [],
+        btnColor: {background: 'red'}
+    }
+
+    componentDidMount() {
+        this.getSaved()
+    }
+    getSaved = () => {
+        API.getAllBooks().then(res => {
+            this.setState({saved: res.data})
+        })
+    }
+    render () {
+        return (
+            <div>
+                <HeaderSaved />
+                <Results books = {this.state.saved}
+                status = "Saved Books: "
+                buttonText = "Delete"
+                buttonColor = {this.state.btnColor}
+                getSaved = {this.getSaved}
+                />
+            </div>
+        )
+    }
+}
+export default Saved

838712e3a82bbcc638e5754c11026489169e2e93
miao0007 <miao188@hotmail.com>
2020-10-10 13:44:53 +1030

    Had Book Schema

---
+++ models/Book.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const BookSchema = new Schema ({
+    title: {
+        type: String,
+        required: TextTrackCue
+    },
+    authors: {
+        type: Array,
+    },
+    description: {
+        type: String,
+    },
+    image: {
+        type: String
+    },
+    link: {
+        type: String,
+        required: true
+    },
+
+    saved: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;
